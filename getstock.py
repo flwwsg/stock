8857a8e9cf4f40cdf594006b893b9de593a50006
flwwsg <2319406132@qq.com>
2017-09-19 17:11:35 +0800

    add next page

--- getstock.py
+++ getstock.py
@@ -2,11 +2,40 @@ from selenium import webdriver
 import time
 from handler import count_stocks
 
+
+def get_top_fund(browser):
+    top_fund_url_xpath = '//div[@class="dbtable"]/table[@id="dbtable"]/tbody/tr/td[3]/a'
+    top_fund_name_xpath = '//div[@class="dbtable"]/table[@id="dbtable"]/tbody/tr/td[4]/a'
+    stack_in_top_fund_xpath = '//div[@class="bd"]/ul/li[@id="position_shares"]/div/table/tbody/tr/td[1]/a'
+    top_fund_urls = browser.find_elements_by_xpath(top_fund_url_xpath)
+    top_fund_names = browser.find_elements_by_xpath(top_fund_name_xpath)
+    print(len(top_fund_urls))
+    stacks = []
+    top_fund = []
+    tmp = webdriver.Chrome()
+    i = 0
+    for iurl, iname in zip(top_fund_urls, top_fund_names):
+        try:
+            index = iurl.get_attribute('href')
+        except Exception:
+            continue
+        print(iurl.text)
+        tmp.get(index)
+        time.sleep(3)
+        ts = tmp.find_elements_by_xpath(
+            stack_in_top_fund_xpath)
+        stacks.append({i.get_attribute('href').split('/')
+                       [3][:-5]: i.text for i in ts})
+        top_fund.append(iname.text)
+    tmp.quit()
+    return top_fund, stacks
+
+
 hybrid_fund = '//div[@class="types"]/ul[@id="types"]/li[contains(text(),"混合")]'
-top_fund_url_xpath = '//div[@class="dbtable"]/table[@id="dbtable"]/tbody/tr/td[3]/a'
-top_fund_name_xpath = '//div[@class="dbtable"]/table[@id="dbtable"]/tbody/tr/td[4]/a'
+
 top_fund_index_xpath = '//div[@class="dbtable"]/table[@id="dbtable"]/thead/tr/th'
-stack_in_top_fund_xpath = '//div[@class="bd"]/ul/li[@id="position_shares"]/div/table/tbody/tr/td[1]/a'
+top_fund_next_page_xpath = '//div[@id="pagebar"]/label[contains(text(), "下一页")]'
+
 start_url = 'http://fund.eastmoney.com/data/fundranking.html'
 browser = webdriver.Chrome()
 
@@ -16,28 +45,15 @@ hy = browser.find_element_by_xpath(hybrid_fund)
 hy.click()
 time.sleep(5)
 
-top_fund_urls = browser.find_elements_by_xpath(top_fund_url_xpath)
-top_fund_names = browser.find_elements_by_xpath(top_fund_name_xpath)
-print(len(top_fund_urls))
-stacks = []
-top_fund = []
-tmp = webdriver.Chrome()
-i = 0
-for iurl, iname in zip(top_fund_urls, top_fund_names):
-    try:
-        index = iurl.get_attribute('href')
-    except Exception:
-        continue
-    print(iurl.text)
-    tmp.get(index)
+reserved = []
+while not reserved:
+    top_fund, stacks = get_top_fund(browser)
+    reserved = count_stocks(top_fund, stacks)
+    next_page = browser.find_element_by_xpath(
+        '//div[@id="pagebar"]/label[contains(text(), "下一页")]')
+    browser.execute_script('arguments[0].click();', next_page)
     time.sleep(3)
-    ts = tmp.find_elements_by_xpath(
-stack_in_top_fund_xpath)
-    stacks.append({i.get_attribute('href').split('/')[3][:-5] :i.text for i in ts})
-    top_fund.append(iname.text)
+    print(top_fund)
+    print(stacks)
 
-tmp.quit()
 browser.quit()
-print(top_fund)
-print(stacks)
-count_stocks(top_fund, stacks)
