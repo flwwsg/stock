5ec96cbdd98cccfeedf6c8f8d18f535954de4c3b
lblue <lblue@linux-ui0z.DHCP>
2018-06-15 00:45:48 +0800

    version 1.0

--- traceStock.py
+++ traceStock.py
@@ -16,6 +16,7 @@ class Stock(Model):
     code = CharField(primary_key=True, max_length=6)
     name = CharField(max_length=40)
     publish_date = DateTimeField(default=datetime.datetime.now())
+    publish_price = FloatField(default=0)
     today_price = FloatField()
     
     class Meta:
@@ -36,9 +37,16 @@ def init_recent_stock():
         db.drop_tables([Stock])
         print("table exists")
     db.create_tables([Stock])
-    stock_list = ['601990', '603666']
+    stock_list = ['603666', '601330', '603486', '603045', '603013', '603259', '603596', '603348', '603733', '603876', '603773', '603301',
+        '603897', '002931', '603214', '002930', '600929', '002928', '002929', '603059', '600901', '603680', '603712', '002927', '603506', '603356','603056',
+        '603655', '002923', '603848', '603809'
+        ]
     for code in stock_list:
-        df = ts.get_realtime_quotes(code)
+        try:
+            df = ts.get_realtime_quotes(code)
+        except Exception:
+            print("error in %s" % code)
+            continue
         row = next(df.iterrows())[1]
         price = row['price']
         name = row['name']
@@ -48,7 +56,9 @@ def init_recent_stock():
             record.save()
         except Stock.DoesNotExist:
             Stock(code=code, name=name, today_price=price).save()
-        print(code, name, price)
+        # print(code, name, price)
+        if float(price) < 30:
+            print(code, name, price)
 
 if __name__ == "__main__":
     init_recent_stock()
